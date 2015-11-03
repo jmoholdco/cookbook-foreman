f5fa2c613f1b646357359790385457ce047cd84e
Morgan Lieberthal <morgan@jmorgan.org>
2015-11-03 15:24:51 -0700

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'morgan@jmorgan.org'
 license 'mit'
 description 'Installs/Configures foreman'
 long_description 'Installs/Configures foreman'
-version '0.1.4'
+version '0.1.5'
 
 depends 'firewalld', '~> 1.1.1'
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,5 +5,4 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 #
 
-yum_package 'epel-release'
 include_recipe 'foreman::firewall_rules'
