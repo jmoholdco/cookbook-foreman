601b05456ae1f945a6b9674c81cf0d93eff6ce71
Morgan Lieberthal <morgan@jmorgan.org>
2015-11-09 17:08:41 -0700

    Don't include CA Certificate Generation by Default

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'morgan@jmorgan.org'
 license 'mit'
 description 'Installs/Configures foreman'
 long_description 'Installs/Configures foreman'
-version '0.2.1'
+version '0.2.2'
 
 depends 'firewalld', '~> 1.1.1'
 depends 'sslcerts', '~> 1.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -15,13 +15,13 @@ cookbook_file '/etc/pki/tls/certs/ca.pem' do
   action :create
 end
 
-ca_certificate 'Foreman CA' do
-  ca_path '/etc/pki/CA'
-  organization 'JML Holdings, LLC'
-  organizational_unit 'Foreman CA'
-  state 'Colorado'
-  city 'Denver'
-  common_name 'JML Foreman CA'
-  type 'intermediate'
-  key_password chef_vault_item('foreman', 'ca_password')['password']
-end
+# ca_certificate 'Foreman CA' do
+#   ca_path '/etc/pki/CA'
+#   organization 'JML Holdings, LLC'
+#   organizational_unit 'Foreman CA'
+#   state 'Colorado'
+#   city 'Denver'
+#   common_name 'JML Foreman CA'
+#   type 'intermediate'
+#   key_password chef_vault_item('foreman', 'ca_password')['password']
+# end
